edb22534ea41afb6a7581e5577ed0d7e6cf34ea6
haloice <nishimukanai@gmail.com>
2020-04-23 13:32:50 +0900

    表示文字列を修正

--- TheConsole/Program.cs
+++ TheConsole/Program.cs
@@ -6,7 +6,7 @@ namespace TheConsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Github!");
         }
     }
 }
