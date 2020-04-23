2256fce0d6c9e471d8f692dea20f8e1725409798
haloice <nishimukanai@gmail.com>
2020-04-23 13:24:26 +0900

    1回目の更新

---
+++ TheConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TheConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
